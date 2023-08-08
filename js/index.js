0ef40dee527aeb15d1568bd95022b439f2e672bc
ZahraCassimjee <231131@virtualwindow.co.za>
2023-08-08 12:12:29 +0200

    work

--- js/index.js
+++ js/index.js
@@ -1,22 +1,44 @@
 const cars = [
-    { model: 'Corolla', topSpeed: 120 },
-    { model: 'Civic', topSpeed: 130 },
-    { model: 'Mustang', topSpeed: 160 },
-    { model: 'Camaro', topSpeed: 170 },
-    { model: 'M3', topSpeed: 180 }
-  ];
+  { model: 'Corolla', topSpeed: 120 },
+  { model: 'Civic', topSpeed: 130 },
+  { model: 'Mustang', topSpeed: 160 },
+  { model: 'Camaro', topSpeed: 170 },
+  { model: 'M3', topSpeed: 180 }
+];
+
+let speed = []
+
+for(let i=0; i<topSpeed.length; i++) {
+  const car = cars[i];
   
-  const filteredByTopSpeed = [];
-  const filteredByModel = [];
+  if(car.topSpeed>150) {
+      speed.push(car)
+  }
+};
+
+console.log(car)
+
+
+
+// const cars = [
+//     { model: 'Corolla', topSpeed: 120 },
+//     { model: 'Civic', topSpeed: 130 },
+//     { model: 'Mustang', topSpeed: 160 },
+//     { model: 'Camaro', topSpeed: 170 },
+//     { model: 'M3', topSpeed: 180 }
+//   ];
   
-  const desiredTopSpeed = 180; 
-  const desiredModel = 'M3'; 
+//   const filteredByTopSpeed = [];
+//   const filteredByModel = [];
   
-  for (let i = 0; i < cars.length; i++) {
-    if (cars[i].topSpeed >= desiredTopSpeed) {
-      filteredByTopSpeed.push(cars[i]);
-    }
-}
+//   const desiredTopSpeed = 180; 
+//   const desiredModel = 'M3'; 
+  
+//   for (let i = 0; i < cars.length; i++) {
+//     if (cars[i].topSpeed >= desiredTopSpeed) {
+//       filteredByTopSpeed.push(cars[i]);
+//     }
+// }
 
   
   
